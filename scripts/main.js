baf403b0e47bf6604c5c7f4c3b90193eba683464
frontbee <hang3126@sina.com>
2016-09-02 20:51:09 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,12 @@
 
+$(function() {
+	function menuToggle() {
+		var $that = $(this);
+
+		$that.toggleClass("open").nextAll("ul").slideToggle(null, function() {
+			$that.one("click", menuToggle);
+		});
+	}
+	
+	$(".parent").one("click", menuToggle);
+});
