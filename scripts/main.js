804aa237a00e8333f8304f34036a78cd3aced53c
frontbee <hang3126@sina.com>
2016-09-06 09:34:46 +0800

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,31 @@
 
 $(function() {
-	function menuToggle() {
-		var $that = $(this);
 
-		$that.toggleClass("open").nextAll("ul").slideToggle(null, function() {
-			$that.one("click", menuToggle);
-		});
+	// 控制左导航条折叠子菜单行为
+
+	function menuScrollToggle() {
+		var $this = $(this);
+
+		$this.toggleClass("open").children("ul").stop(1, 0).slideToggle();
+		if ($this.hasClass("open")) {
+			$this.siblings(".open").removeClass("open")
+			.children("ul").stop(1, 0).slideUp();
+		}
 	}
-	
-	$(".parent").one("click", menuToggle);
+
+	function menuToggle() {
+		$(this).children("ul").toggle();
+	}
+
+	$("#menu").delegate(".parent.scroll", "click", menuScrollToggle);
+	$("#menu").delegate(".parent.toggle", "mouseenter mouseleave", menuToggle);
+
+	// 弹出缩小左导航条
+
+	$("#nav-button").click(function() {
+		$("#left-column").toggleClass("active");
+		$("#menu").children(".parent").toggleClass("scroll").toggleClass("toggle");
+		$("#menu").find("ul").css("display", "none");
+		$(".open").removeClass("open");
+	})
 });
